4d56dd0400666f3b8e1929d6aa481dbd52bcb860
Daljeet6 <162506530+Daljeet6@users.noreply.github.com>
2024-03-06 21:44:28 +0000

    Add files via upload

---
+++ voters/guide.php
@@ -0,0 +1,84 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Voters Panel - Voter Guide</title>
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+
+<style type="text/css">
+    .center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+
+</style>
+
+<body>
+    
+    <div class="container-fluid">
+        <div class="row bg-black text-white">
+            <div class="col-1"> 
+                <img src="../assets/images/box.gif" width="80px"/>
+            </div>
+            <div class="col-11 my-auto">
+                <h3> ONLINE VOTING SYSTEM </h3>
+            </div>
+        </div>
+        <div class="row bg-green">
+    <div class="col-12 bg-green">
+    <nav class="navbar navbar-expand-lg navbar-light bg-green">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link text-white mr-3" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white mr-3" href="guid.php">Voter Guide</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white mr-3" href="../admin/logout.php">Logout</a>
+        </li>
+         </ul>
+    </div>
+    </nav>
+</div>
+</div>
+
+<h1 style="text-align: center;">Welcome to Our user Voting Guide</h1>
+<p style="text-align: center;">Please follow the instructions carefully provided below before casting the vote to your desired candidate.</p>
+<p class="text-danger" style="text-align: center;">Warning! Once you casted vote you can not change your decision.</p>
+
+<img src="../assets/images/guide.JPG" class="center" />
+
+
+
+
+
+
+
+
+<div class="row bg-black text-center text-white fixed-bottom">
+        <div class="col-12 my-1">
+        <p> &copy; Copyright 2024 - All Rights Reserved  <br />
+                Developed by Daljeet - MSc Computer Science Project
+            </p>
+        </div>
+    </div>
+
+</div>
+
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ voters/inc/footer.php
@@ -0,0 +1,15 @@
+
+    <div class="row bg-black text-center text-white fixed-bottom">
+        <div class="col-12 my-1">
+        <p> &copy; Copyright 2024 - All Rights Reserved  <br />
+                Developed by Daljeet - MSc Computer Science Project
+            </p>
+        </div>
+    </div>
+
+</div>
+
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+</body>
+</html>
